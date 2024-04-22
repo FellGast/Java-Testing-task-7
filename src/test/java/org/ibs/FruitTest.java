3dbc10cc78bde1d7c9075322af064dd0397deef0
FellGast <antonshhavelev@gmail.com>
2024-04-22 21:04:42 +0300

    1.1

--- src/test/java/org/ibs/FruitTest.java
+++ src/test/java/org/ibs/FruitTest.java
@@ -1,21 +1,42 @@
 package org.ibs;
 
+import io.restassured.RestAssured;
 import org.ibs.api.FoodPojo;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.sessionId;
 import static io.restassured.http.ContentType.*;
 import static org.hamcrest.Matchers.*;
 
 public class FruitTest {
-    private final static String URL = "http://localhost:8080/";
 
+    @BeforeEach
+
+    public void setUp() {
+        updateSessionId();
+        // Базовый URL вашего API
+        RestAssured.baseURI = "http://localhost:8080/";
+    }
+    public void updateSessionId() {
+        // Получаем идентификатор сессии из переменной среды
+        sessionId = System.getenv("SESSION_ID");
+        if (sessionId == null) {
+            // В случае, если переменная среды не установлена, выводим ошибку и завершаем выполнение
+            System.err.println("Ошибка: Переменная среды SESSION_ID не установлена.");
+            System.exit(1);
+        }
+        // Базовый URL вашего API
+        RestAssured.baseURI = "http://localhost:8080/";
+    }
     @Test
     void testFruit() {
+        updateSessionId();
         given()
-                .cookie("JSESSIONID", "59466C32B723995232CDACED53EF1F02")
+                .cookie("JSESSIONID", sessionId)
                 .body("{\n" +
                         "  \"name\": \"Банан\",\n" +
                         "  \"type\": \"FRUIT\",\n" +
@@ -24,35 +45,37 @@ public class FruitTest {
                 .when()
                 .contentType(JSON)
                 .log().all()
-                .post(URL + "api/food")
+                .post("api/food")
                 .then()
                 .log().all()
                 .statusCode(200);
+        updateSessionId();
         given()
-                .cookie("JSESSIONID", "59466C32B723995232CDACED53EF1F02")
+                .cookie("JSESSIONID", sessionId)
                 .when()
                 .contentType(JSON)
-                .get(URL + "api/food")
+                .get("api/food")
                 .then()
                 .log().all()
                 .assertThat()
                 .statusCode(200)
                 .body("name", hasItem("Банан"));
+        updateSessionId();
         given()
-                .cookie("JSESSIONID", "59466C32B723995232CDACED53EF1F02")
+                .cookie("JSESSIONID", sessionId)
                 .when()
                 .contentType(JSON)
-                .post(URL + "api/data/reset")
+                .post("api/data/reset")
                 .then()
                 .log().all()
                 .assertThat()
                 .statusCode(200);
         List<FoodPojo> foodList = given()
-                .cookie("JSESSIONID", "59466C32B723995232CDACED53EF1F02")
+                .cookie("JSESSIONID", sessionId)
                 .when()
                 .contentType(JSON)
                 .log().all()
-                .get(URL + "api/food")
+                .get("api/food")
                 .then()
                 .log().all()
                 .extract()
--- src/test/java/org/ibs/VegetableTest.java
+++ src/test/java/org/ibs/VegetableTest.java
@@ -1,21 +1,42 @@
 package org.ibs;
 
+import io.restassured.RestAssured;
 import org.ibs.api.FoodPojo;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.http.ContentType.JSON;
-import static org.hamcrest.Matchers.hasItem;
+import static io.restassured.RestAssured.sessionId;
+import static io.restassured.http.ContentType.*;
+import static org.hamcrest.Matchers.*;
 
 public class VegetableTest {
-    private final static String URL = "http://localhost:8080/";
 
+    @BeforeEach
+
+    public void setUp() {
+        updateSessionId();
+        // Базовый URL вашего API
+        RestAssured.baseURI = "http://localhost:8080/";
+    }
+    public void updateSessionId() {
+        // Получаем идентификатор сессии из переменной среды
+        sessionId = System.getenv("SESSION_ID");
+        if (sessionId == null) {
+            // В случае, если переменная среды не установлена, выводим ошибку и завершаем выполнение
+            System.err.println("Ошибка: Переменная среды SESSION_ID не установлена.");
+            System.exit(1);
+        }
+        // Базовый URL вашего API
+        RestAssured.baseURI = "http://localhost:8080/";
+    }
     @Test
     void testFruit() {
+        updateSessionId();
         given()
-                .cookie("JSESSIONID", "59466C32B723995232CDACED53EF1F02")
+                .cookie("JSESSIONID", sessionId)
                 .body("{\n" +
                         "  \"name\": \"Картофель\",\n" +
                         "  \"type\": \"VEGETABLE\",\n" +
@@ -24,35 +45,37 @@ public class VegetableTest {
                 .when()
                 .contentType(JSON)
                 .log().all()
-                .post(URL + "api/food")
+                .post("api/food")
                 .then()
                 .log().all()
                 .statusCode(200);
+        updateSessionId();
         given()
-                .cookie("JSESSIONID", "59466C32B723995232CDACED53EF1F02")
+                .cookie("JSESSIONID", sessionId)
                 .when()
                 .contentType(JSON)
-                .get(URL + "api/food")
+                .get("api/food")
                 .then()
                 .log().all()
                 .assertThat()
                 .statusCode(200)
                 .body("name", hasItem("Картофель"));
+        updateSessionId();
         given()
-                .cookie("JSESSIONID", "59466C32B723995232CDACED53EF1F02")
+                .cookie("JSESSIONID", sessionId)
                 .when()
                 .contentType(JSON)
-                .post(URL + "api/data/reset")
+                .post("api/data/reset")
                 .then()
                 .log().all()
                 .assertThat()
                 .statusCode(200);
         List<FoodPojo> foodList = given()
-                .cookie("JSESSIONID", "59466C32B723995232CDACED53EF1F02")
+                .cookie("JSESSIONID", sessionId)
                 .when()
                 .contentType(JSON)
                 .log().all()
-                .get(URL + "api/food")
+                .get("api/food")
                 .then()
                 .log().all()
                 .extract()
@@ -61,4 +84,5 @@ public class VegetableTest {
                 .getList("$", FoodPojo.class);
 
     }
+
 }
